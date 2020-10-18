6db91b14a0be9b81ef35c978328adac1ca906f21
Tushar Kumar <tr.jakhara019@gmail.com>
2020-10-18 17:07:55 +0530

    know-the-project template added

--- hello.py
+++ hello.py
@@ -1,15 +1,17 @@
 import os
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 import random
 
 app = Flask(__name__)
 
 
-nucleotide = ["A","G","T","C"]
+nucleotide = ["A", "G", "T", "C"]
+
 
 @app.route('/')
 def hello():
     return render_template("index.html")
+
 
 @app.route('/', methods=['POST'])
 def hello1():
@@ -17,18 +19,25 @@ def hello1():
         return render_template("index.html", error="Number has to be greater then 0")
     len = request.form['len']
     seq = ''.join([random.choice(nucleotide)
-               for nuc in range(int(len))])
+                   for nuc in range(int(len))])
     rev_seq = seq[::-1]
-    rev_seq = rev_seq.replace("A","t").replace("T","a").replace("G","c").replace("C","g").upper()
+    rev_seq = rev_seq.replace("A", "t").replace(
+        "T", "a").replace("G", "c").replace("C", "g").upper()
     # processed_text = text.upper()
     x = 1
-    return render_template("index.html", seq = seq , rev_seq = rev_seq , x=x)
+    return render_template("index.html", seq=seq, rev_seq=rev_seq, x=x)
+
+
+@app.route('/know-the-project/')
+def know_the_project():
+    return render_template("about.html")
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug="True")
-
+    app.run(debug="True")
